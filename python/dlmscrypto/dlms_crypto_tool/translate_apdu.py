010fd9e2d13859c938aa906c34893133b2139af0
Ricky <riccardocasaro@yahoo.it>
2025-05-24 11:38:36 +0200

    - Modified xdlms_tag.py - Implemented function translate_apdu

--- python/dlmscrypto/dlms_crypto_tool/translate_apdu.py
+++ python/dlmscrypto/dlms_crypto_tool/translate_apdu.py
@@ -1,10 +1,24 @@
 from xdlms_tag import *
 
-def translate_apdu(apdu: str) -> str:
+def translate_apdu(apdu: str) -> list[str]:
 
-    for i in range(0, len(apdu), 2):
+    apdu_field = [] # It will contain the translated APDU
 
-        apdu_element = apdu[i:i+2]
+    if TAGS.get(apdu[0:2],"Command not supported!") == "GET_REQUEST_TAG":
 
+        apdu_field.append(TAGS.get(apdu[0:2], "Command not supported!"))
+        apdu_field.append(TAGS.get(apdu[2:4], "Command not supported!"))
+        apdu_field.append(TAGS.get(apdu[4:6], "Command not supported!"))
+        apdu_field.append(TAGS.get(apdu[6:10], "Command not supported!"))
+        apdu_field.append(TAGS.get(apdu[10:22], "Command not supported!"))
+        apdu_field.append(TAGS.get(apdu[22:24], "Command not supported!"))
 
-    return apdu
+    elif TAGS.get(apdu[0:2],"Command not supported!") == "SET_REQUEST_TAG":
+
+        print("")
+
+    else:
+
+        print("")
+
+    return apdu_field
